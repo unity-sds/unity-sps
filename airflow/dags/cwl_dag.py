eb86f96cf77b64b524e5f1f79814df8ee60c7d45
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-02-29 10:27:07 -0700

    Adding the SBG Preprocess no-cwl workflow

--- airflow/dags/cwl_dag.py
+++ airflow/dags/cwl_dag.py
@@ -3,6 +3,7 @@
 # The "cwl-runner" tool is invoked to execute the CWL workflow.
 # Parameter cwl_workflow: the URL of the CWL workflow to execute.
 # Parameter args_as_json: JSON string contained the specific values for the workflow specific inputs.
+import json
 from datetime import datetime
 from airflow import DAG
 from kubernetes.client import models as k8s
@@ -18,8 +19,14 @@ POD_TEMPLATE_FILE = "/opt/airflow/dags/docker_cwl_pod.yaml"
 POD_NAMESPACE = "airflow"
 
 # Example arguments
-default_cwl_workflow = "https://raw.githubusercontent.com/unity-sds/unity-sps-prototype/cwl-docker/cwl/cwl_workflows/echo_from_docker.cwl"
-default_args_as_json = '{ "greeting": "Ciao", "name": "Terra" }'
+default_cwl_workflow = "https://raw.githubusercontent.com/unity-sds/sbg-workflows/main/preprocess/sbg-preprocess-workflow.cwl"
+default_args_as_json_dict = {"input_processing_labels": ["label1", "label2"],
+                             "input_cmr_stac": "https://cmr.earthdata.nasa.gov/search/granules.stac?collection_concept_id=C2408009906-LPCLOUD&temporal[]=2023-08-10T03:41:03.000Z,2023-08-10T03:41:03.000Z",
+                             "input_unity_dapa_client": "40c2s0ulbhp9i0fmaph3su9jch",
+                             "input_unity_dapa_api": "https://d3vc8w9zcq658.cloudfront.net",
+                             "input_crid": "001",
+                             "output_collection_id": "urn:nasa:unity:unity:dev:SBG-L1B_PRE___1",
+                             "output_data_bucket": "sps-dev-ds-storage"}
 
 # Default DAG configuration
 dag_default_args = {
@@ -39,7 +46,7 @@ dag = DAG(dag_id='cwl-dag',
           default_args=dag_default_args,
           params={
               "cwl_workflow": Param(default_cwl_workflow, type="string"),
-              "args_as_json": Param(default_args_as_json, type="string"),
+              "args_as_json": Param(json.dumps(default_args_as_json_dict), type="string"),
           })
 
 # Environment variables
