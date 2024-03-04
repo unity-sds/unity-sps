3b1adbdbb9b03fcb8f5d882e2e793f3680ffbeec
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-03-04 07:35:14 -0700

    [CHANGE] Supporting passing CWL parameters from URL

--- airflow/dags/cwl_dag.py
+++ airflow/dags/cwl_dag.py
@@ -48,8 +48,14 @@ dag = DAG(
     max_active_runs=1,
     default_args=dag_default_args,
     params={
-        "cwl_workflow": Param(default_cwl_workflow, type="string"),
-        "args_as_json": Param(json.dumps(default_args_as_json_dict), type="string"),
+        "cwl_workflow": Param(default_cwl_workflow,
+                              type="string",
+                              title="CWL workflow",
+                              description="The CWL workflow URL"),
+        "args_as_json": Param(json.dumps(default_args_as_json_dict),
+                              type="string",
+                              title="CWL wokflow parameters",
+                              description="The job parameters encodes as a JSON string, or the URL of a JSON or YAML file"),
     },
 )
 
--- airflow/docker/cwl/docker_cwl_entrypoint.sh
+++ airflow/docker/cwl/docker_cwl_entrypoint.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 # Script to execute a CWL workflow that includes Docker containers
 # The Docker engine is started before the CWL execution, and stopped afterwards.
-# $1: the CWL workflow URL (example: https://raw.githubusercontent.com/unity-sds/unity-sps-prototype/cwl-docker/cwl/cwl_workflows/echo_from_docker.cwl)
-# $2: the CWL job parameters as a JSON formatted string (example: { name: John Doe })
+# $1: the CWL workflow URL
+#     (example: https://github.com/unity-sds/sbg-workflows/blob/main/L1-to-L2-e2e.cwl)
+# $2: a) the CWL job parameters as a JSON formatted string
+#        (example: { "name": "John Doe" })
+#  OR b) The URL of a YAML or JSON file containing the job parameters
+#        (example: https://github.com/unity-sds/sbg-workflows/blob/main/L1-to-L2-e2e.dev.yml)
 # $3: optional output directory, defaults to the current directory
 # Note: $output_dir must be accessible by the Docker container that executes this script
 
-set -ex
+set -e
 cwl_workflow=$1
 job_args=$2
 output_dir=${3:-.}
+
+# switch between the 2 cases a) and b) for job_args
+if [ "$job_args" = "${job_args#{}" ]
+then
+  # job_args does NOT start with '{'
+  echo "Using job arguments from URL: $job_args"
+else
+  echo "$job_args" > /tmp/job_args.json
+  echo "Using job arguments from JSON string:" && cat /tmp/job_args.json
+  job_args="/tmp/job_args.json"
+fi
 echo "Executing CWL workflow: $cwl_workflow with json arguments: $job_args and output directory: $output_dir"
-echo "$job_args" > /tmp/job_args.json
-cat /tmp/job_args.json
 
 # create output directory if it doesn't exist
 mkdir -p "$output_dir"
