6c99216a6b8db6969194b98e4c647fc13cad3896
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-03-15 08:51:09 -0700

    Update ref for cs infra eks module

--- terraform-unity/modules/terraform-eks-cluster/locals.tf
+++ terraform-unity/modules/terraform-eks-cluster/locals.tf
@@ -2,16 +2,4 @@ locals {
   counter              = var.counter != "" ? var.counter : random_id.counter.hex
   resource_name_prefix = join("-", compact([var.project, var.venue, var.service_area, "%s", var.deployment_name, local.counter]))
   cluster_name         = format(local.resource_name_prefix, "eks")
-  # common_tags = {
-  #   Name        = ""
-  #   Venue       = var.venue
-  #   Proj        = var.project
-  #   ServiceArea = var.service_area
-  #   CapVersion  = var.release
-  #   Component   = ""
-  #   CreatedBy   = var.service_area
-  #   Env         = var.venue
-  #   mission     = var.project
-  #   Stack       = ""
-  # }
 }
