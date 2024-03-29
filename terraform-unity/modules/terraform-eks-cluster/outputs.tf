60cb758c9a4222c50a3d08b15df255ba23dfc7db
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2024-03-29 14:47:42 -0700

    Initial work at wrapping the sps-airflow install
    Adding an outputs file to the terraform-eks-cluster module so it can
    be (ab)used by the preinstall script when installing eks and preparing
    for the airflow requirements

---
+++ terraform-unity/modules/terraform-eks-cluster/outputs.tf
@@ -0,0 +1,9 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = cluster_name
+}
+
+output "counter" {
+  description = "SPS-EKS cluster counter"
+  value       = counter
+}
