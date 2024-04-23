6c57ab737c2159bd0f80875c6940c51ea71ae54b
Drew Meyers <drew.meyers@jpl.nasa.gov>
2024-04-23 09:39:39 -0700

    Remove unused provider

--- terraform-unity/versions.tf
+++ terraform-unity/versions.tf
@@ -21,10 +21,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.43.0"
     }
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "2.0.4"
-    }
     time = {
       source  = "hashicorp/time"
       version = "0.11.1"
