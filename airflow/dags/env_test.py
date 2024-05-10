05d169c0c030867fd4e075f2fb58845e29dea2d4
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2024-05-10 11:05:02 -0700

    i don't know how to write dags

--- airflow/dags/env_test.py
+++ airflow/dags/env_test.py
@@ -18,29 +18,25 @@ default_args = {
     "start_date": datetime.utcfromtimestamp(0),
 }
 
-
-dump_env = BashOperator(
-    task_id="env",
-    bash_command="env",
-)
-
-check_vars = BashOperator(
-    task_id="check",
-    bash_command="echo {} {} {} {} {}".format(
-        Variable.get("unity_project"),
-        Variable.get("unity_venue"),
-        Variable.get("unity_deployment_name"),
-        Variable.get("unity_counter"),
-        Variable.get("unity_cluster_name")),
-)
-
-
-dag = DAG(
+with DAG(
     dag_id="env_test",
     default_args=default_args,
     schedule=None,
     is_paused_upon_creation=False,
     tags=["test"],
-)
+) as dag:
+    dump_env = BashOperator(
+        task_id="env",
+        bash_command="env",
+    )
 
-dump_env > check_vars
+    check_vars = BashOperator(
+        task_id="check",
+        bash_command="echo {} {} {} {} {}".format(
+            Variable.get("unity_project"),
+            Variable.get("unity_venue"),
+            Variable.get("unity_deployment_name"),
+            Variable.get("unity_counter"),
+            Variable.get("unity_cluster_name")),
+    )
+    dump_env >> check_vars
