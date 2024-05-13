26f2e30f1badbdc03628249ef4f33df88098681b
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-05-13 08:54:59 -0600

    Using busybox

--- airflow/dags/karpenter_test.py
+++ airflow/dags/karpenter_test.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 
+from kubernetes.client import models as k8s
 from unity_sps_utils import get_node_affinity
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 POD_NAMESPACE = "airflow"
+
+
+CONTAINER_RESOURCES = k8s.V1ResourceRequirements(requests={"cpu": "3"})
 
 default_args = {
     "owner": "unity-sps",
@@ -28,47 +32,56 @@ dag = DAG(
 # Define KubernetesPodOperator tasks with default affinity
 compute_task = KubernetesPodOperator(
     namespace=POD_NAMESPACE,
-    image="hello-world",
+    image="busybox",
+    cmds=["sleep"],
+    arguments=["60"],
     name="compute-task",
     task_id="compute_task",
     get_logs=True,
     dag=dag,
-    is_delete_operator_pod=True,
+    labels={"task-type": "standalone"},
+    # on_finish_action="keep_pod",
+    is_delete_operator_pod=False,
+    # container_resources=CONTAINER_RESOURCES,
     node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
-    affinity=get_node_affinity(
-        capacity_type=["spot"], instance_family=["c6i", "c5"], instance_cpu=["4", "2"]
-    ),
-    startup_timeout_seconds=900,
+    affinity=get_node_affinity(capacity_type=["spot"], instance_family=["c6i"], instance_cpu=["4"]),
+    startup_timeout_seconds=1800,
 )
 
 memory_task = KubernetesPodOperator(
     namespace=POD_NAMESPACE,
-    image="hello-world",
+    image="busybox",
+    cmds=["sleep"],
+    arguments=["60"],
     name="memory-task",
     task_id="memory_task",
     get_logs=True,
     dag=dag,
-    is_delete_operator_pod=True,
+    labels={"task-type": "standalone"},
+    # on_finish_action="keep_pod",
+    is_delete_operator_pod=False,
+    # container_resources=CONTAINER_RESOURCES,
     node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
-    affinity=get_node_affinity(
-        capacity_type=["spot"], instance_family=["r6i", "r5"], instance_cpu=["4", "2"]
-    ),
-    startup_timeout_seconds=900,
+    affinity=get_node_affinity(capacity_type=["spot"], instance_family=["r6i"], instance_cpu=["4"]),
+    startup_timeout_seconds=1800,
 )
 
 general_task = KubernetesPodOperator(
     namespace=POD_NAMESPACE,
-    image="hello-world",
+    image="busybox",
+    cmds=["sleep"],
+    arguments=["60"],
     name="general-task",
     task_id="general_task",
     get_logs=True,
     dag=dag,
-    is_delete_operator_pod=True,
+    labels={"task-type": "standalone"},
+    # container_resources=CONTAINER_RESOURCES,
+    # on_finish_action="keep_pod",
+    is_delete_operator_pod=False,
     node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
-    affinity=get_node_affinity(
-        capacity_type=["spot"], instance_family=["m6i", "m5"], instance_cpu=["4", "2"]
-    ),
-    startup_timeout_seconds=900,
+    affinity=get_node_affinity(capacity_type=["spot"], instance_family=["m6i"], instance_cpu=["4"]),
+    startup_timeout_seconds=1800,
 )
 
 # Task sequence
