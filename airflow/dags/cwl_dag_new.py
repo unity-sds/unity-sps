a3b27071d31fc894358f7446d1f582ce10862c53
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-06-13 09:28:44 -0600

    Updating the CWL DAG to avoid using the Pod template file, and other improvements.

---
+++ airflow/dags/cwl_dag_new.py
@@ -0,0 +1,132 @@
+"""
+DAG to execute a generic CWL workflow.
+
+The Airflow KubernetesPodOperator starts a Docker container that includes the Docker engine and the CWL libraries.
+The "cwl-runner" tool is invoked to execute the CWL workflow.
+Parameter cwl_workflow: the URL of the CWL workflow to execute.
+Parameter args_as_json: JSON string contained the specific values for the workflow specific inputs.
+"""
+
+import json
+import logging
+import os
+import shutil
+from datetime import datetime
+
+from airflow.models.baseoperator import chain
+from airflow.models.param import Param
+from airflow.operators.python import PythonOperator, get_current_context
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.utils.trigger_rule import TriggerRule
+from kubernetes.client import models as k8s
+
+from airflow import DAG
+
+# The Kubernetes namespace within which the Pod is run (it must already exist)
+POD_NAMESPACE = "airflow"
+
+# The path of the working directory where the CWL workflow is executed
+# (aka the starting directory for cwl-runner).
+# This is fixed to the EFS /scratch directory in this DAG.
+WORKING_DIR = "/scratch"
+
+# default parameters
+DEFAULT_CWL_WORKFLOW = (
+    "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/main/demos/echo_message.cwl"
+)
+DEFAULT_CWL_ARGUMENTS = {"message": "Hello Unity"}
+
+# Default DAG configuration
+dag_default_args = {
+    "owner": "unity-sps",
+    "depends_on_past": False,
+    "start_date": datetime.utcfromtimestamp(0),
+}
+
+# common parameters
+INPUT_PROCESSING_LABELS = ["label1", "label2"]
+
+dag = DAG(
+    dag_id="cwl_dag_new",
+    description="CWL DAG",
+    tags=["CWL"],
+    is_paused_upon_creation=False,
+    catchup=False,
+    schedule=None,
+    max_active_runs=10,
+    max_active_tasks=30,
+    default_args=dag_default_args,
+    params={
+        "cwl_workflow": Param(
+            DEFAULT_CWL_WORKFLOW, type="string", title="CWL workflow", description="The CWL workflow URL"
+        ),
+        "cwl_args": Param(
+            json.dumps(DEFAULT_CWL_ARGUMENTS),
+            type="string",
+            title="CWL workflow parameters",
+            description="The job parameters encoded as a JSON string, or the URL of a JSON or YAML file",
+        ),
+    },
+)
+
+
+def setup(ti=None, **context):
+    """
+    Task that creates the working directory on the shared volume.
+    """
+    context = get_current_context()
+    dag_run_id = context["dag_run"].run_id
+    local_dir = f"/shared-task-data/{dag_run_id}"
+    logging.info(f"Creating directory: {local_dir}")
+    os.makedirs(local_dir, exist_ok=True)
+    logging.info(f"Created directory: {local_dir}")
+
+
+setup_task = PythonOperator(task_id="Setup", python_callable=setup, dag=dag)
+
+cwl_task = KubernetesPodOperator(
+    retries=0,
+    task_id="cwl_task",
+    namespace=POD_NAMESPACE,
+    name="cwl-task-pod",
+    image="ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.0.0",
+    service_account_name="airflow-worker",
+    in_cluster=True,
+    get_logs=True,
+    startup_timeout_seconds=1200,
+    arguments=["{{ params.cwl_workflow }}", "{{ params.cwl_args }}"],
+    container_security_context={"privileged": True},
+    # container_resources=CONTAINER_RESOURCES,
+    container_logs=True,
+    volume_mounts=[
+        k8s.V1VolumeMount(name="workers-volume", mount_path=WORKING_DIR, sub_path="{{ dag_run.run_id }}")
+    ],
+    volumes=[
+        k8s.V1Volume(
+            name="workers-volume",
+            persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="airflow-kpo"),
+        )
+    ],
+    dag=dag,
+)
+
+
+def cleanup(**context):
+    """
+    Tasks that deletes all data shared between Tasks
+    from the Kubernetes PersistentVolume
+    """
+    dag_run_id = context["dag_run"].run_id
+    local_dir = f"/shared-task-data/{dag_run_id}"
+    if os.path.exists(local_dir):
+        shutil.rmtree(local_dir)
+        logging.info(f"Deleted directory: {local_dir}")
+    else:
+        logging.info(f"Directory does not exist, no need to delete: {local_dir}")
+
+
+cleanup_task = PythonOperator(
+    task_id="Cleanup", python_callable=cleanup, dag=dag, trigger_rule=TriggerRule.ALL_DONE
+)
+
+chain(setup_task, cwl_task, cleanup_task)
