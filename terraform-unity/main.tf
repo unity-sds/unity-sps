031fde37648db3167bd3b09e16dafb64aad51f8b
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-07-01 04:26:05 -0600

    Adding S3 backend for Karpenter and SPS

--- terraform-unity/main.tf
+++ terraform-unity/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    # full path to Terraform state file:
+    # s3://<bucket>/<key>
+    bucket = ""
+    key = ""
+    region = "us-west-2"
+  }
+}
+
 module "unity-sps-airflow" {
   source                     = "./modules/terraform-unity-sps-airflow"
   project                    = var.project
