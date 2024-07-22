5ecee7d91f6710f93339af7d0a3c411ee9514f06
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-07-22 11:46:44 -0600

    Working version with selectable fields

--- airflow/dags/cwl_dag.py
+++ airflow/dags/cwl_dag.py
@@ -19,7 +19,6 @@ from airflow.operators.python import PythonOperator, get_current_context
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.utils.trigger_rule import TriggerRule
 from kubernetes.client import models as k8s
-from unity_sps_utils import get_affinity
 
 from airflow import DAG
 
@@ -27,6 +26,9 @@ from airflow import DAG
 POD_NAMESPACE = "airflow"
 POD_LABEL = "cwl_task"
 SPS_DOCKER_CWL_IMAGE = "ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.1.0"
+
+NODE_POOL_DEFAULT = "airflow-kubernetes-pod-operator"
+NODE_POOL_HIGH_WORKLOAD = "airflow-kubernetes-pod-operator-high-workload"
 
 # The path of the working directory where the CWL workflow is executed
 # (aka the starting directory for cwl-runner).
@@ -38,6 +40,44 @@ DEFAULT_CWL_WORKFLOW = (
     "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/main/demos/echo_message.cwl"
 )
 DEFAULT_CWL_ARGUMENTS = json.dumps({"message": "Hello Unity"})
+
+
+class SpsKubernetesPodOperator(KubernetesPodOperator):
+    """
+    Subclass of KubernetesPodOperator that adds more fields for Jinja templating.
+    """
+
+    template_fields = KubernetesPodOperator.template_fields + ("node_selector", "affinity")
+
+
+def get_affinity2(capacity_type: str, anti_affinity_label: str):
+
+    affinity = k8s.V1Affinity(
+        node_affinity=k8s.V1NodeAffinity(
+            required_during_scheduling_ignored_during_execution=k8s.V1NodeSelector(
+                node_selector_terms=[
+                    k8s.V1NodeSelectorTerm(
+                        match_expressions=[
+                            {"key": "karpenter.sh/capacity-type", "operator": "In", "values": [capacity_type]}
+                        ]
+                    )
+                ]
+            ),
+        ),
+        pod_anti_affinity=k8s.V1PodAntiAffinity(
+            required_during_scheduling_ignored_during_execution=[
+                k8s.V1PodAffinityTerm(
+                    k8s.V1LabelSelector(
+                        match_expressions=[{"key": "app", "operator": "In", "values": [anti_affinity_label]}]
+                    ),
+                    topology_key="kubernetes.io/hostname",
+                ),
+            ]
+        ),
+    )
+
+    return affinity
+
 
 # Alternative arguments to execute SBG Pre-Process
 # DEFAULT_CWL_WORKFLOW =  "https://raw.githubusercontent.com/unity-sds/sbg-workflows/main/preprocess/sbg-preprocess-workflow.cwl"
@@ -53,7 +93,9 @@ CONTAINER_RESOURCES = k8s.V1ResourceRequirements(
     requests={
         # "cpu": "2660m",  # 2.67 vCPUs, specified in milliCPUs
         # "memory": "22Gi",  # Rounded to 22 GiB for easier specification
-        "ephemeral-storage": "10Gi"
+        "memory": "{{ params.request_memory }}",
+        "cpu": "{{ params.request_cpu }} ",
+        "ephemeral-storage": "{{ params.request_storage }} ",
     },
     # limits={
     #    # "cpu": "2660m",  # Optional: set the same as requests if you want a fixed allocation
@@ -68,9 +110,6 @@ dag_default_args = {
     "depends_on_past": False,
     "start_date": datetime.utcfromtimestamp(0),
 }
-
-# common parameters
-INPUT_PROCESSING_LABELS = ["label1", "label2"]
 
 dag = DAG(
     dag_id="cwl_dag",
@@ -92,6 +131,24 @@ dag = DAG(
             title="CWL workflow parameters",
             description="The job parameters encoded as a JSON string, or the URL of a JSON or YAML file",
         ),
+        "request_memory": Param(
+            "4Gi",
+            type="string",
+            enum=["8Gi", "16Gi", "32Gi", "64Gi", "128Gi", "256Gi"],
+            title="Docker container memory",
+        ),
+        "request_cpu": Param(
+            "4",
+            type="string",
+            enum=["2", "4", "8", "16", "32"],
+            title="Docker container CPU",
+        ),
+        "request_storage": Param(
+            "10Gi",
+            type="string",
+            enum=["10Gi", "50Gi", "100Gi", "200Gi", "300Gi"],
+            title="Docker container storage",
+        ),
     },
 )
 
@@ -107,10 +164,24 @@ def setup(ti=None, **context):
     os.makedirs(local_dir, exist_ok=True)
     logging.info(f"Created directory: {local_dir}")
 
+    # select the node pool based on what resources were requested
+    node_pool = NODE_POOL_DEFAULT
+    storage = context["params"]["request_storage"]  # 100Gi
+    storage = int(storage[0:-2])  # 100
+    memory = context["params"]["request_memory"]  # 32Gi
+    memory = int(memory[0:-2])  # 32
+    cpu = int(context["params"]["request_cpu"])  # 8
+
+    logging.info(f"Requesting storage={storage}Gi memory={memory}Gi CPU={cpu}")
+    if (storage > 30) or (memory > 32) or (cpu > 8):
+        node_pool = NODE_POOL_HIGH_WORKLOAD
+    logging.info(f"Selecting node pool={node_pool}")
+    ti.xcom_push(key="node_pool", value=node_pool)
+
 
 setup_task = PythonOperator(task_id="Setup", python_callable=setup, dag=dag)
 
-cwl_task = KubernetesPodOperator(
+cwl_task = SpsKubernetesPodOperator(
     retries=0,
     task_id="cwl_task",
     namespace=POD_NAMESPACE,
@@ -134,14 +205,10 @@ cwl_task = KubernetesPodOperator(
         )
     ],
     dag=dag,
-    node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
+    node_selector={"karpenter.sh/nodepool": "{{ti.xcom_pull(task_ids='Setup', key='node_pool')}}"},
     labels={"app": POD_LABEL},
     annotations={"karpenter.sh/do-not-disrupt": "true"},
-    affinity=get_affinity(
-        capacity_type=["spot"],
-        instance_type=["r7i.xlarge"],
-        anti_affinity_label=POD_LABEL,
-    ),
+    affinity=get_affinity2("spot", POD_LABEL),
     on_finish_action="keep_pod",
     is_delete_operator_pod=False,
 )
