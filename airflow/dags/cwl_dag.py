a4b99933e73a5eaf4616c9dd4553e3d4ac9e086e
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2024-09-05 11:48:25 -0600

    Setting tag to 2.2.0-beta-1

--- airflow/dags/cwl_dag.py
+++ airflow/dags/cwl_dag.py
@@ -25,7 +25,7 @@ from airflow import DAG
 # The Kubernetes namespace within which the Pod is run (it must already exist)
 POD_NAMESPACE = "sps"
 POD_LABEL = "cwl_task"
-SPS_DOCKER_CWL_IMAGE = "ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.2.0"
+SPS_DOCKER_CWL_IMAGE = "ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.2.0-beta-1"
 
 NODE_POOL_DEFAULT = "airflow-kubernetes-pod-operator"
 NODE_POOL_HIGH_WORKLOAD = "airflow-kubernetes-pod-operator-high-workload"
