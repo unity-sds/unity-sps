290669f00845e65302fdd409214c980f961fe588
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2024-10-06 18:51:22 -0700

    adjustments to work with marketplace requirements

--- terraform-unity/modules/terraform-unity-sps-eks/main.tf
+++ terraform-unity/modules/terraform-unity-sps-eks/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 module "unity-eks" {
-  source          = "git@github.com:unity-sds/unity-cs-infra.git//terraform-unity-eks_module?ref=unity-sps-2.2.0-hotfix"
+  source          = "git::https://github.com/unity-sds/unity-cs-infra.git//terraform-unity-eks_module?ref=unity-sps-2.2.0-hotfix"
   deployment_name = local.cluster_name
   nodegroups      = var.nodegroups
   aws_auth_roles = [{
