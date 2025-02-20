a5fd5bdacadc0f032fb1d5e8bf81990c21981116
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-02-20 17:16:16 -0500

    Ensure consistent use of log level in CWL DAG

--- airflow/plugins/unity_sps_utils.py
+++ airflow/plugins/unity_sps_utils.py
@@ -13,7 +13,10 @@ POD_NAMESPACE = "sps"  # The Kubernetes namespace within which the Pod is run (i
 POD_LABEL = "cwl_task" + datetime.now().strftime(
     "%Y%m%d_%H%M%S_%f"
 )  # unique pod label to assure each job runs on its own pod
-SPS_DOCKER_CWL_IMAGE = "ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.5.0"
+
+# Note: each Pod is assigned the same label to assure that (via the anti-affinity requirements)
+# two Pods with the same label cannot run on the same Node
+SPS_DOCKER_CWL_IMAGE = "ghcr.io/unity-sds/unity-sps/sps-docker-cwl:2.5.3"
 
 NODE_POOL_DEFAULT = "airflow-kubernetes-pod-operator"
 NODE_POOL_HIGH_WORKLOAD = "airflow-kubernetes-pod-operator-high-workload"
