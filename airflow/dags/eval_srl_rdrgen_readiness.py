d6794ef99d28bd54ccff0cb57e60bd307c645bad
pymonger <pymonger@gmail.com>
2025-02-27 20:08:43 +0000

    update

--- airflow/dags/eval_srl_rdrgen_readiness.py
+++ airflow/dags/eval_srl_rdrgen_readiness.py
@@ -18,16 +18,17 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 default_args = {"owner": "unity-sps", "start_date": datetime.utcfromtimestamp(0)}
 
+# match only ECM prodType
 FNAME_RE = re.compile(
-    r"^(?P<id>(?P<instrument>SA|SB|FL|FR)(?P<color>[A-GJ-MORTX-Z_])(?P<specFlag>[A-Z_])(?P<primaryTime>\d{4})(?P<spacer0>[A-Z_])(?P<secondaryTime>\d{10})(?P<spacer1>_)(?P<tertiaryTime>\d{3})(?P<prodType>[A-Z_]{3})(?P<geometry>[NT])(?P<seqId>[A-Z]{3}[A-Z_]\d{5})(?P<downsample>[0-3_])(?P<compression>[A-Z0-9]{2})(?P<producer>[A-Z_])(?P<version>[A-Z0-9_]{2}))(?P<extension>\.VIC)$"
+    r"^(?P<id>(?P<instrument>SA|SB|FL|FR)(?P<color>[A-GJ-MORTX-Z_])(?P<specFlag>[A-Z_])(?P<primaryTime>\d{4})(?P<spacer0>[A-Z_])(?P<secondaryTime>\d{10})(?P<spacer1>_)(?P<tertiaryTime>\d{3})(?P<prodType>ECM)(?P<geometry>[NT])(?P<seqId>[A-Z]{3}[A-Z_]\d{5})(?P<downsample>[0-3_])(?P<compression>[A-Z0-9]{2})(?P<producer>[A-Z_])(?P<version>[A-Z0-9_]{2}))(?P<extension>\.VIC)$"
 )
 
 
 with DAG(
-    dag_id="eval_srl_vic2png",
+    dag_id="eval_srl_rdrgen",
     default_args=default_args,
     schedule=None,
-    tags=["eval_srl_vic2png"],
+    tags=["eval_srl_rdrgen"],
     params={
         "payload": Param(
             "s3://unity-gmanipon-ads-deployment-dev/output/SAM_0000_0734432789_658ECMNAUT_040960LUJ01.VIC",
@@ -37,7 +38,7 @@ with DAG(
 ) as dag:
 
     @task
-    def evaluate_vic2png(params: dict):
+    def evaluate_rdrgen(params: dict):
         context = get_current_context()
         dag_run_id = context["dag_run"].run_id
         s3_hook = S3Hook()
@@ -51,45 +52,48 @@ with DAG(
         # ensure matches filename convention and parse filename components
         match = FNAME_RE.search(fname)
         if not match:
-            raise AirflowFailException("Filename {fname} not recognized.")
+            return {"success": False, "vic_url": None, "id": match.groupdict()["id"], "message": "File didn't match regex."}
 
         # build expected file prefixes (TODO: fix hardcoding)
         keys = {"vic": (bucket, key)}
 
-        # default vic2png DAG parameters
-        vic2png_args = {"success": True, "vic_url": None, "id": match.groupdict()["id"]}
+        # default rdrgen DAG parameters
+        rdrgen_args = {"success": True, "vic_url": None, "id": match.groupdict()["id"]}
 
         # check if all inputs exist
         for k, v in keys.items():
             exists = s3_hook.check_for_key(v[1], bucket_name=v[0])
-            vic2png_args["success"] &= exists
+            rdrgen_args["success"] &= exists
             if exists:
-                vic2png_args[f"{k}_url"] = f"s3://{v[0]}/{v[1]}"
+                rdrgen_args[f"{k}_url"] = f"s3://{v[0]}/{v[1]}"
 
         # return params and evaluation result
-        return vic2png_args
+        return rdrgen_args
 
-    evaluate_vic2png_task = evaluate_vic2png()
+    evaluate_rdrgen_task = evaluate_rdrgen()
 
     @task.short_circuit()
-    def vic2png_evaluation_successful():
+    def rdrgen_evaluation_successful():
         context = get_current_context()
-        print(f"{context['ti'].xcom_pull(task_ids='evaluate_vic2png')}")
-        return context["ti"].xcom_pull(task_ids="evaluate_vic2png")["success"]
+        print(f"{context['ti'].xcom_pull(task_ids='evaluate_rdrgen')}")
+        rdrgen_args = context["ti"].xcom_pull(task_ids="evaluate_rdrgen")
+        if not rdrgen_args["success"] and "message" in rdrgen_args:
+            print(f"{rdrgen_args['message']}")
+        return rdrgen_args["success"]
 
-    vic2png_evaluation_successful_task = vic2png_evaluation_successful()
+    rdrgen_evaluation_successful_task = rdrgen_evaluation_successful()
 
-    trigger_vic2png_task = TriggerDagRunOperator(
-        task_id="trigger_vic2png",
-        trigger_dag_id="vic2png",
+    trigger_rdrgen_task = TriggerDagRunOperator(
+        task_id="trigger_rdrgen",
+        trigger_dag_id="rdrgen",
         # uncomment the next line if we want to dedup dagRuns for a particular ID
-        trigger_run_id="{{ ti.xcom_pull(task_ids='evaluate_vic2png')['id'] }}-vic2png",
+        trigger_run_id="{{ ti.xcom_pull(task_ids='evaluate_rdrgen')['id'] }}-rdrgen",
         trigger_rule=TriggerRule.ALL_SUCCESS,
         skip_when_already_exists=True,
         conf={
-            "vic_url": "{{ ti.xcom_pull(task_ids='evaluate_vic2png')['vic_url'] }}",
-            "output_url": "s3://gmanipon-dev-sps-isl/STACAM/PNG",
+            "vic_url": "{{ ti.xcom_pull(task_ids='evaluate_rdrgen')['vic_url'] }}",
+            "output_url": "s3://gmanipon-dev-sps-isl/STACAM/VIC",
         },
     )
 
-    evaluate_vic2png_task >> vic2png_evaluation_successful_task >> trigger_vic2png_task
+    evaluate_rdrgen_task >> rdrgen_evaluation_successful_task >> trigger_rdrgen_task
