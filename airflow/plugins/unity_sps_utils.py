adfb923af843226d979a3d7f06abc8da3081acd8
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2025-04-02 16:15:08 -0600

    Fixing the re-introduced bug of POD_LABEL

--- airflow/plugins/unity_sps_utils.py
+++ airflow/plugins/unity_sps_utils.py
@@ -3,16 +3,13 @@ Module containing common utilities for the Unity Science Processing System.
 """
 
 import os
-from datetime import datetime
 
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 
 # Shared constants
 POD_NAMESPACE = "sps"  # The Kubernetes namespace within which the Pod is run (it must already exist)
-POD_LABEL = "cwl_task" + datetime.now().strftime(
-    "%Y%m%d_%H%M%S_%f"
-)  # unique pod label to assure each job runs on its own pod
+POD_LABEL = "cwl_task"
 
 # Note: each Pod is assigned the same label to assure that (via the anti-affinity requirements)
 # two Pods with the same label cannot run on the same Node
