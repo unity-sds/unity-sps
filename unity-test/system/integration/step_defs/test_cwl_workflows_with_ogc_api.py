ecdecafaab80904f9e00ab32b2ab8bfb74a78908
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-16 14:15:39 -0400

    Update CWL DAG arguments to remove ecr and add in log levels

--- unity-test/system/integration/step_defs/test_cwl_workflows_with_ogc_api.py
+++ unity-test/system/integration/step_defs/test_cwl_workflows_with_ogc_api.py
@@ -28,10 +28,10 @@ CWL_DAG_DATA = {
             "cwl_args": {
                 "dev": "https://raw.githubusercontent.com/GodwinShen/emit-ghg/refs/heads/main/test/emit-ghg-dev.json"
             },
+            "log_level": "INFO",
             "request_storage": "100Gi",
             # r7i.2xlarge: 8 CPUs, 64 GB memory
             "request_instance_type": "r7i.2xlarge",
-            "use_ecr": False,
         },
         "outputs": {"result": {"transmissionMode": "reference"}},
     },
@@ -45,11 +45,11 @@ CWL_DAG_DATA = {
                 "test": "https://raw.githubusercontent.com/unity-sds/sbg-workflows/main/preprocess"
                 "/sbg-preprocess-workflow.test.yml",
             },
+            "log_level": "INFO",
             "request_storage": "10Gi",
             # c6i.xlarge: 4vCPUs, 8 GB memory
             # r7i.xlarge: 4 CPUs 32 GB memory
             "request_instance_type": "r7i.xlarge",
-            "use_ecr": False,
         },
         "outputs": {"result": {"transmissionMode": "reference"}},
     },
@@ -61,10 +61,10 @@ CWL_DAG_DATA = {
                 "dev": "https://raw.githubusercontent.com/unity-sds/"
                 "sbg-workflows/refs/heads/main/L1-to-L2-e2e.dev.yml",
             },
+            "log_level": "INFO",
             "request_storage": "100Gi",
             # c6i.8xlarge: 32 CPUs, 64 GB memory
             "request_instance_type": "c6i.8xlarge",
-            "use_ecr": False,
         },
         "outputs": {"result": {"transmissionMode": "reference"}},
     },
