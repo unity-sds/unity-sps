fe530c6a1a820e7f77a3f41f07e5bd43a7dc8e6f
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-22 08:52:26 -0400

    Temp hold

--- airflow/dags/cwl_dag_modular.py
+++ airflow/dags/cwl_dag_modular.py
@@ -41,8 +41,8 @@ from unity_sps_utils import (
 from airflow import DAG
 
 # Task constants
-STAGE_IN_WORKFLOW = "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/refs/heads/307-log-levels/demos/stage_in_log_level.cwl"
-STAGE_OUT_WORKFLOW = "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/refs/heads/307-log-levels/demos/stage_out_cwl_log_level.cwl"
+STAGE_IN_WORKFLOW = "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/refs/heads/uds-version-update/demos/cwl_dag_modular_stage_in.cwl"
+STAGE_OUT_WORKFLOW = "https://raw.githubusercontent.com/unity-sds/unity-sps-workflows/refs/heads/uds-version-update/demos/cwl_dag_modular_stage_out.cwl"
 LOCAL_DIR = "/shared-task-data"
 
 # The path of the working directory where the CWL workflow is executed
--- unity-test/system/integration/step_defs/test_cwl_workflows_with_airflow_api.py
+++ unity-test/system/integration/step_defs/test_cwl_workflows_with_airflow_api.py
@@ -42,7 +42,7 @@ DAG_PARAMETERS = {
             "process_workflow": "http://awslbdockstorestack-lb-1429770210.us-west-2.elb.amazonaws.com:9998/api/ga4gh/trs/v2/tools/%23workflow%2Fdockstore.org%2Fbrianlee731%2FSBG-unity-isofit-mod_test/versions/14/PLAIN-CWL/descriptor/%2Fprocess.cwl",
             "process_args": json.dumps({}),
             "log_level": "INFO",
-            "request_instance_type": "c6i.8xlarge",
+            "request_instance_type": "r7i.2xlarge",
             "request_storage": "100Gi",
         },
     },
