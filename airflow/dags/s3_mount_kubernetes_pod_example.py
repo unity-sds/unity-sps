042d0af6137442824cb058126a496aead9f5a195
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2025-04-28 07:29:21 -0600

    Adding task to example dag

--- airflow/dags/s3_mount_kubernetes_pod_example.py
+++ airflow/dags/s3_mount_kubernetes_pod_example.py
@@ -1,7 +1,11 @@
+from airflow.models.baseoperator import chain
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 
 from airflow import DAG
+
+# Use a Docker image that has access to S3 (and has bash or Python)
+DOCKER_IMAGE = "amazonlinux:2"
 
 with DAG(
     dag_id="s3_mount_kubernetes_pod_example",
@@ -21,11 +25,30 @@ with DAG(
         name="s3-mount-volume", mount_path="/mnt/s3", read_only=False  # Path inside the container
     )
 
-    task = KubernetesPodOperator(
-        task_id="list_s3_from_pod",
-        name="s3-access-task",
+    write_task = KubernetesPodOperator(
+        task_id="write_s3_from_pod",
+        name="s3-write-task",
         namespace="sps",
-        image="amazonlinux:2",  # or any image that can read S3 (and has bash or Python)
+        image=DOCKER_IMAGE,
+        cmds=["sh", "-c"],
+        arguments=["echo 'Hello from the container!' >> /mnt/s3/$(date -u).txt"],
+        volumes=[volume],
+        volume_mounts=[volume_mount],
+        get_logs=True,
+        is_delete_operator_pod=True,
+        service_account_name="airflow-worker",
+        startup_timeout_seconds=1800,
+        container_security_context={"privileged": True},
+        owner="unity-sps",
+        node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
+        annotations={"karpenter.sh/do-not-disrupt": "true"},
+    )
+
+    list_task = KubernetesPodOperator(
+        task_id="list_s3_from_pod",
+        name="s3-list-task",
+        namespace="sps",
+        image=DOCKER_IMAGE,
         cmds=["sh", "-c"],
         arguments=["ls /mnt/s3"],  # just listing contents for demo
         volumes=[volume],
@@ -39,3 +62,5 @@ with DAG(
         node_selector={"karpenter.sh/nodepool": "airflow-kubernetes-pod-operator"},
         annotations={"karpenter.sh/do-not-disrupt": "true"},
     )
+
+    chain(write_task, list_task)
