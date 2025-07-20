8a0f593aad23a889dc5d4e192c56a1ad2c033321
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2025-07-19 21:59:52 -0700

    adjusting module source

--- terraform-unity/modules/terraform-unity-sps-eks/main.tf
+++ terraform-unity/modules/terraform-unity-sps-eks/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 module "unity-eks" {
-  source          = "git::https://github.com/unity-sds/unity-cs-infra.git//terraform-unity-eks_module"
+  source          = "git::https://github.com/unity-sds/unity-cs-infra.git//terraform-unity-eks_module?ref=lunsfo-sps-eks-1.32-troubleshooting"
   deployment_name = local.cluster_name
   project         = var.project
   venue           = var.venue
