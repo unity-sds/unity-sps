35555826a81ddb6ce8e9aa398cf2dce1e119f258
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-08-19 11:23:54 -0700

    webserver_config.py updates and added user_auth

--- airflow/config/webserver_config.py
+++ airflow/config/webserver_config.py
@@ -1,3 +1,128 @@
-# Issue 404: DISABLE AIRRLOW AUTHENTICATION (https://github.com/unity-sds/unity-sps/issues/404)
-# Disable airflow authentication, https://airflow.apache.org/docs/apache-airflow-providers-fab/stable/auth-manager/webserver-authentication.html
-AUTH_ROLE_PUBLIC = "Admin"
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Default configuration for the Airflow webserver"""
+from __future__ import annotations
+import os
+import logging
+import jwt
+import requests
+from base64 import b64decode
+from cryptography.hazmat.primitives import serialization
+from tokenize import Exponent
+from airflow.www.fab_security.manager import AUTH_OAUTH
+from airflow.www.security import AirflowSecurityManager
+from flask_appbuilder import expose
+from flask_appbuilder.security.views import AuthOAuthView
+basedir = os.path.abspath(os.path.dirname(__file__))
+log = logging.getLogger(__name__)
+APP_THEME = "simplex.css"
+# Flask-WTF flag for CSRF
+WTF_CSRF_ENABLED = True
+# ----------------------------------------------------
+# AUTHENTICATION CONFIG
+# ----------------------------------------------------
+# For details on how to set up each of the following authentication, see
+# http://flask-appbuilder.readthedocs.io/en/latest/security.html# authentication-methods
+# for details.
+AUTH_TYPE = AUTH_OAUTH
+# Uncomment to setup Full admin role name
+# AUTH_ROLE_ADMIN = 'Admin'
+# Uncomment and set to desired role to enable access without authentication
+AUTH_ROLE_PUBLIC = 'Viewer'
+# Will allow user self registration
+AUTH_USER_REGISTRATION = True
+# The recaptcha it's automatically enabled for user self registration is active and the keys are necessary
+# RECAPTCHA_PRIVATE_KEY = PRIVATE_KEY
+# RECAPTCHA_PUBLIC_KEY = PUBLIC_KEY
+# Config for Flask-Mail necessary for user self registration
+# MAIL_SERVER = 'smtp.gmail.com'
+# MAIL_USE_TLS = True
+# MAIL_USERNAME = 'yourappemail@gmail.com'
+# MAIL_PASSWORD = 'passwordformail'
+# MAIL_DEFAULT_SENDER = 'sender@gmail.com'
+# The default user self registration role
+AUTH_USER_REGISTRATION_ROLE = "Public"
+AUTH_ROLES_SYNC_AT_LOGIN = True
+AUTH_ROLES_MAPPING = {
+  "airflow_admin": ["Admin"],
+  "airflow_op": ["Op"],
+  "airflow_user": ["User"],
+  "airflow_viewer": ["Viewer"],
+  "airflow_public": ["Public"],
+}
+PROVIDER_NAME = 'keycloak'
+CLIENT_ID = 'airflow'
+CLIENT_SECRET = 'ADD LATER'
+OIDC_ISSUER = 'https://dit.kc-test-maap.xyz/realms/MAAP'
+OIDC_BASE_URL = "{oidc_issuer}/protocol/openid-connect".format(oidc_issuer=OIDC_ISSUER)
+OIDC_TOKEN_URL = "{oidc_base_url}/token".format(oidc_base_url=OIDC_BASE_URL)
+OIDC_AUTH_URL = "{oidc_base_url}/auth".format(oidc_base_url=OIDC_BASE_URL)
+# When using OAuth Auth, uncomment to setup provider(s) info
+OAUTH_PROVIDERS = [{
+    'name':PROVIDER_NAME,
+    'token_key':'access_token',
+    'icon':'fa-circle-o',
+    'remote_app': {
+        'api_base_url':OIDC_BASE_URL,
+        'access_token_url':OIDC_TOKEN_URL,
+        'authorize_url':OIDC_AUTH_URL,
+        'request_token_url': None,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'client_kwargs':{
+            'scope': 'email profile'
+        },
+    }
+}]
+req = requests.get(OIDC_ISSUER)
+key_der_base64 = req.json()["public_key"]
+key_der = b64decode(key_der_base64.encode())
+public_key = serialization.load_der_public_key(key_der)
+class CustomAuthRemoteUserView(AuthOAuthView):
+    @expose("/logout/")
+    def logout(self):
+        """Delete access token before logging out."""
+        return super().logout()
+class CustomSecurityManager(AirflowSecurityManager):
+    authoauthview = CustomAuthRemoteUserView
+  
+    def oauth_user_info(self, provider, response):
+        if provider == PROVIDER_NAME:
+            token = response["access_token"]
+            me = jwt.decode(token, public_key, algorithms=['HS256', 'RS256'], audience=CLIENT_ID)
+            # sample of resource_access
+            # {
+            #   "resource_access": { "airflow": { "roles": ["airflow_admin"] }}
+            # }
+            groups = me["resource_access"]["airflow"]["roles"] # unsafe
+            if len(groups) < 1:
+                groups = ["airflow_public"]
+            else:
+                groups = [str for str in groups if "airflow" in str]
+            userinfo = {
+                "username": me.get("preferred_username"),
+                "email": me.get("email"),
+                "first_name": me.get("given_name"),
+                "last_name": me.get("family_name"),
+                "role_keys": groups,
+            }
+            log.info("user info: {0}".format(userinfo))
+            return userinfo
+        else:
+            return {}
+SECURITY_MANAGER_CLASS = CustomSecurityManager
---
+++ airflow/plugins/user_auth.py
@@ -0,0 +1,93 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+   User authentication backend
+   Referencies
+     - https://flask-appbuilder.readthedocs.io/en/latest/_modules/flask_appbuilder/security/manager.html
+     - https://github.com/apache/airflow/blob/main/airflow/api/auth/backend/basic_auth.py
+"""
+from __future__ import annotations
+from functools import wraps
+from typing import Any, Callable, TypeVar, cast
+from flask import Response, request
+from flask_appbuilder.const import AUTH_OAUTH, AUTH_LDAP, AUTH_DB
+from flask_login import login_user
+from airflow.utils.airflow_flask_app import get_airflow_app
+from airflow.www.fab_security.sqla.models import User
+import logging
+import jwt
+import requests
+from base64 import b64decode
+from cryptography.hazmat.primitives import serialization
+CLIENT_AUTH: tuple[str, str] | Any | None = None
+log = logging.getLogger(__name__)
+CLIENT_ID = 'airflow'
+OIDC_ISSUER = 'https://dit.kc-test-maap.xyz/realms/MAAP'
+req = requests.get(OIDC_ISSUER)
+key_der_base64 = req.json()["public_key"]
+key_der = b64decode(key_der_base64.encode())
+public_key = serialization.load_der_public_key(key_der)
+def init_app(_):
+    """Initializes authentication backend"""
+T = TypeVar("T", bound=Callable)
+def auth_current_user() -> User | None:
+    """Authenticate and set current user if Authorization header exists"""
+    
+    ab_security_manager = get_airflow_app().appbuilder.sm
+    user = None
+    if ab_security_manager.auth_type == AUTH_OAUTH:
+        if not request.headers['Authorization']:
+            return None
+        
+        token = str.replace(str(request.headers['Authorization']), 'Bearer ', '')
+        me = jwt.decode(token, public_key, algorithms=['HS256', 'RS256'], audience=CLIENT_ID)
+        groups = me["resource_access"]["airflow"]["roles"] # unsafe
+        if len(groups) < 1:
+            groups = ["airflow_public"]
+        else:
+            groups = [str for str in groups if "airflow" in str]
+        userinfo = {
+            "username": me.get("preferred_username"),
+            "email": me.get("email"),
+            "first_name": me.get("given_name"),
+            "last_name": me.get("family_name"),
+            "role_keys": groups,
+        }
+        user = ab_security_manager.auth_user_oauth(userinfo)
+    else:
+        auth = request.authorization
+        if auth is None or not auth.username or not auth.password:
+            return None
+        if ab_security_manager.auth_type == AUTH_LDAP:
+            user = ab_security_manager.auth_user_ldap(auth.username, auth.password)
+        if ab_security_manager.auth_type == AUTH_DB:
+            user = ab_security_manager.auth_user_db(auth.username, auth.password)
+        log.info("user: {0}".format(user))
+        if user is not None:
+            login_user(user, remember=False)
+        return user
+def requires_authentication(function: T):
+# def requires_authentication():
+    """Decorator for functions that require authentication"""
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        if auth_current_user() is not None:
+            return function(*args, **kwargs)
+        else:
+            return Response("Unauthorized", 401, {"WWW-Authenticate": "Basic"})
+        
+    return cast(T, decorated)
